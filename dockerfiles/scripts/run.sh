c07702fc32c8f228ee951bd126b7b21733bc9863
Pedro Alves <pedro.alves@webdetails.pt>
2017-08-17 18:40:40 +0100

    - More work done for the volumes, etc

--- dockerfiles/scripts/run.sh
+++ dockerfiles/scripts/run.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 
+APPLICATION=$(basename /opt/*)
+
+if [ ! -f /root/INIT_DONE ]
+then
+
+	echo Running the init scripts
+	find /opt -iname common-funcs.sh -exec chmod +w {} \; -exec  perl -pi -e 's/-v "\$INSTALLDIR":"\$INTERNALHOMEDIR"/-v \$(hostname)-volume:\/opt/' {} \; -exec chmod -w {} \;
+	
+	cd /opt/$APPLICATION
+	bin/${APPLICATION}_setup
+	
+	touch /root/INIT_DONE
+
+fi
+
+
 # 1. Extract the software file
 
 
-# cd /opt/lumada && \
-# 	tar -xvf ./*tgz -C . && \
-# 	./*/bin/install && \
-# 	echo DONE!
 
 if [ -z "$DEBUG" ]; then
   echo Starting Lumada in normal mode
